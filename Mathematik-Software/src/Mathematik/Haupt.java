c5d453257c1ad40f84f36781354b7c90c4dbe502
ferhat44 <ferhat44@ws368-201-13.szut.uni-bremen.de>
2020-12-09 08:28:55 +0100

    wallah ka

--- Mathematik-Software/src/Mathematik/Haupt.java
+++ Mathematik-Software/src/Mathematik/Haupt.java
@@ -3,7 +3,7 @@ package Mathematik;
 public class Haupt {
 
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println("");
 		
 	}
 
---
+++ Mathematik-Software/src/Matrizenrechnung/MultiplikationmiteinerZahl.java
@@ -0,0 +1,10 @@
+package Matrizenrechnung;
+
+public class MultiplikationmiteinerZahl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

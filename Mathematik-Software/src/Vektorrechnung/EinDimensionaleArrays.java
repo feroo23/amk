9cf9c0a65a7753c0bed2d12fe2cb0c45806ac65f
ferhat44 <ferhat44@ws368-201-13.szut.uni-bremen.de>
2020-12-09 09:21:30 +0100

    3

---
+++ Mathematik-Software/src/Vektorrechnung/EinDimensionaleArrays.java
@@ -0,0 +1,18 @@
+package Vektorrechnung;
+
+public class EinDimensionaleArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public static double[] Multiplikation() {
+		
+		
+		
+		return 0;
+		
+	}
+}
